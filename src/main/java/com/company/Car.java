55e160e4aa815ec7ce0a480d5c1c193c5adc56e6
Abssollutte <heavens.lightnings@gmail.com>
2025-11-05 19:21:43 +0300

    -release new feature (again)

---
+++ src/main/java/com/company/Car.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Car {
+    private int speed;
+    private String brand;
+
+}
